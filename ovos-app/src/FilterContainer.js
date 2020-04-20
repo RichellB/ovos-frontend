05504179553b00477baeeea438a9497563e434c3
richell <richell.bonfield12@gmail.com>
2020-04-19 20:40:12 -0500

    Added Search Bar Function

---
+++ ovos-app/src/FilterContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FilterContainer(){
+
+  return (
+    <div id="search-bar">
+      Filter: <input></input>
+    </div>
+  )
+}
+
+export default FilterContainer
